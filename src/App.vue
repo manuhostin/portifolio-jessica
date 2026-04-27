fd5afc2d8a6db209260f890b13a5f3482b1871a2
manuhostin <hostimmanu@gmail.com>
2026-04-27 16:27:47 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HeaderComp from './components/HeaderComp.vue'
+import FooterComp from './components/FooterComp.vue'
+</script>
+
+<template>
+<HeaderComp />
+<br>
+<FooterComp />
+</template>
+
+<style>
+body {
+  background-color: black;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  background-color: black;
+  min-height: 100vh;
+}
+</style>
+
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,43 @@
+<template>
+<header>
+<nav>
+  <a href="" class="logo">Jess Wilbert</a>
+  <div class="links">
+    <a href="">SOBRE</a>
+    <a href="">CONTATO</a>
+  </div>
+</nav>
+</header>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+header {
+  color: #fff;
+  padding: 1rem;
+}
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.logo {
+  font-weight: 300;
+  margin-right: 0;
+}
+a {
+  display: inline-block;
+  color: #fff;
+  text-decoration: none;
+  margin-right: 1rem;
+  font-family: Poppins, sans-serif;
+  transition: color 0.3s ease;
+}
+
+a:hover {
+  color: #bbbbbb;
+}
+</style>
+
+<script setup></script>
